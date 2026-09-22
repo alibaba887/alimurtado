dee043d34fa58f1e42ba587a7a1ac2c6eb1dff9f
Moh. Ali Murtado <alibaba@digasdev.com>
2026-09-22 02:29:09 +0000

    feat(seo): add lastmod, changefreq, and active filter to sitemap.xml

--- styles/site/default/sitemap.php
+++ styles/site/default/sitemap.php
@@ -2,6 +2,8 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc><?php echo base_url(); ?></loc>
+        <lastmod><?php echo date('Y-m-d'); ?></lastmod>
+        <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
     <url>
@@ -14,10 +16,15 @@
             <priority>0.8</priority>
         </url>
     <?php } ?>
-    <?php foreach ($posts as $item) { ?>
+    <?php foreach ($posts as $item) { 
+        if (isset($item->display) && $item->display === '0') continue;
+        $post_date = !empty($item->datetime) ? date('c', strtotime($item->datetime)) : date('c');
+    ?>
         <url>
             <loc><?php echo site_url('post/' . $item->blog_id . '-' . sanitize($item->title)); ?></loc>
-            <priority>0.6</priority>
+            <lastmod><?php echo $post_date; ?></lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
         </url>
     <?php } ?>
 </urlset>
